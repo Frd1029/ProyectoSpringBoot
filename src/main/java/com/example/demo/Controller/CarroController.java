fd73aa3f86c30c5122c6811a2872ed7e1ec05b3a
Frd1029 <freduar1029@gmail.com>
2023-12-08 18:19:54 -0500

    Se crearon metodos en el CarroController getCarroById

--- src/main/java/com/example/demo/Controller/CarroController.java
+++ src/main/java/com/example/demo/Controller/CarroController.java
@@ -1,15 +1,19 @@
 package com.example.demo.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Entity.Carro;
 import com.example.demo.Repositorio.CarroRepositorio;
 
-// Controlador para la entidad Carro
+
 @RestController
 @RequestMapping("/carros")
 public class CarroController {
@@ -22,6 +26,21 @@ public class CarroController {
     @GetMapping
     public List<Carro> getCarroAll() {
         return carroRepositorio.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Carro getCarroById(@PathVariable Integer id){
+        Optional<Carro> carro = carroRepositorio.findById(id);
+        if (carro.isPresent()) {
+            return carro.get();
+        }
+        return null;
+    }
+
+    @PostMapping
+    public Carro postCarros(@RequestBody Carro carro){
+        carroRepositorio.save(carro);
+        return carro;
     }
 
 }
