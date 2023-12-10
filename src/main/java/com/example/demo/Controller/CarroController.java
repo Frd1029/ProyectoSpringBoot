442bf1350b04e35af6c1f10ce42960a18e3fe9de
Frd1029 <freduar1029@gmail.com>
2023-12-10 12:13:43 -0500

    Se agregan los metodos putCarroById() y deleteCarroById(), en la clase CarroController.

--- src/main/java/com/example/demo/Controller/CarroController.java
+++ src/main/java/com/example/demo/Controller/CarroController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,35 @@ public class CarroController {
         return carro;
     }
 
-    //Prueba del commit
+    @PutMapping("/{id}")
+    public Carro putCarroById(@PathVariable Integer id, @RequestBody Carro carro){
+        Optional<Carro> carroBase = carroRepositorio.findById(id); 
+        if (carroBase.isPresent()) {
+            Carro carroActualizado = carroBase.get();
+
+            carroActualizado.setPlaca(carro.getPlaca());
+            carroActualizado.setColor(carro.getColor());
+            carroActualizado.setMarca(carro.getMarca());
+            carroActualizado.setModelo(carro.getModelo());
+            carroActualizado.setCilindraje(carro.getCilindraje());
+			
+            carroRepositorio.save(carroActualizado);
+			
+			return carroActualizado;
+        }
+        return null;
+    }
+
+    
+    public Carro deleteCarroById(@PathVariable Integer id){
+        Optional<Carro> carro = carroRepositorio.findById(id);
+        if (carro.isPresent()) {
+            Carro carroReturn = carro.get();
+            carroRepositorio.deleteById(id);
+            
+            return carroReturn;
+        }
+        return null;
+    }
 
 }
