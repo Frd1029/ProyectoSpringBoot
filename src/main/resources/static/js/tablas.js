78412f47e4435cd8bef1fb7f069be09b68a3b1da
Frd1029 <freduar1029@gmail.com>
2023-12-11 01:41:30 -0500

    Se crea un style.css sencillo para que se vea más visual la implementación del index.html

--- src/main/resources/static/js/tablas.js
+++ src/main/resources/static/js/tablas.js
@@ -3,26 +3,26 @@ function getData(endpoint) {
         .then(response => response.json())
         .then(data => {
             const resultadoDiv = document.getElementById('resultado');
-            resultadoDiv.innerHTML = ''; // Limpiar el contenido actual
+            resultadoDiv.innerHTML = '';
 
-            const table = document.createElement('table');
+            const tabla = document.createElement('table');
 
-            const headerRow = table.insertRow(0);
-            for (const key in data[0]) {
-                const th = document.createElement('th');
-                th.textContent = key;
-                headerRow.appendChild(th);
-            }
+            const encabezados = tabla.insertRow(0);
+                for (const key in data[0]) {
+                    const th = document.createElement('th');
+                    th.textContent = key;
+                    encabezados.appendChild(th);
+                }
 
             data.forEach(item => {
-                const row = table.insertRow();
-                for (const key in item) {
-                    const cell = row.insertCell();
-                    cell.textContent = item[key];
-                }
+                const fila = tabla.insertRow();
+                    for (const key in item) {
+                        const celda = fila.insertCell();
+                        celda.textContent = item[key];
+                    }
             });
 
-            resultadoDiv.appendChild(table);
+            resultadoDiv.appendChild(tabla);
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error! al enviar la petición', error));
 }
